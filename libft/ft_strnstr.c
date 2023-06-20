a38a905e95140242128b962f0015a4e716a3b274
David Oller Mas <doller-m@cbr11s2.42barcelona.com>
2023-06-20 12:07:47 +0200

    lib fet

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,11 +6,79 @@
 /*   By: doller-m <doller-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 13:06:02 by doller-m          #+#    #+#             */
-/*   Updated: 2023/06/16 15:06:05 by doller-m         ###   ########.fr       */
+/*   Updated: 2023/06/20 12:05:39 by doller-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+
+int	flag(char *aguja, char *pajar, size_t i, size_t len)
+{
+	size_t	j;	
+
+	j = 0;
+	while (i < len && pajar[i] != 0)
+	{
+		if (aguja[0] != pajar[i])
+			i++;
+		else if (aguja[0] == pajar[i])
+		{
+			j = 0;
+			while (aguja[j] == pajar[i + j] && (i + j < len))
+			{
+				j++;
+				if (aguja[j] == '\0')
+					return (1);
+			}
+		i++;
+		}
+	}
+	return (-1);
+}
+
+size_t	loop_inici(char *aguja, char *pajar, size_t i, size_t len)
+{
+	size_t	j;
+
+	if (aguja[0] == pajar[i])
+	{
+		j = 0;
+		while (aguja[j] == pajar[i + j] && (i + j < len))
+		{
+			j++;
+			if (aguja[j] == '\0')
+				return (i);
+			if (pajar[i] == '\0')
+				return (0);
+		}
+		i++;
+	}
+	return (0);
+}
+
+size_t	loop_2(char *aguja, char *pajar, size_t i, size_t len)
+{
+	size_t	j;	
+
+	j = 0;
+	while (i < len && pajar[i] != 0)
+	{
+		if (aguja[0] != pajar[i])
+			i++;
+		else if (aguja[0] == pajar[i])
+		{
+			j = 0;
+			while (aguja[j] == pajar[i + j] && (i + j < len))
+			{
+				j++;
+				if (aguja[j] == '\0')
+					return (i);
+			}
+		i++;
+		}
+	}
+	return (0);
+}
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
@@ -25,39 +93,15 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	j = 0;
 	if (*needle == '\0')
 		return ((char *)haystack);
-	if (aguja[0] == pajar[i])
-	{
-		j = 0;
-		while (aguja[j] == pajar[i + j] && (i + j < len))
-		{
-			j++;
-			if (aguja[j] == '\0')
-				return (&pajar[i]);
-			if (pajar[i] == '\0')
-				return (0);
-		}
-		i++;
-	}
-	loop(i, j, *aguja, *pajar, len);
-	while (i < len && pajar[i] != 0)
-	{
-		if (aguja[0] != pajar[i])
-			i++;
-		else if (aguja[0] == pajar[i])
-		{
-			j = 0;
-			while (aguja[j] == pajar[i + j] && (i + j < len))
-			{
-				j++;
-				if (aguja[j] == '\0')
-					return (&pajar[i]);
-			}
-		i++;
-		}
-	}
-	return (0);
+	if (flag(aguja, pajar, i, len) == -1)
+		return (0);
+	i = loop_inici(aguja, pajar, i, len);
+	if (i > 0)
+		return (&pajar[i]);
+	i = loop_2(aguja, pajar, i, len);
+	return (&pajar[i]);
 }
-/*
+/* 
 #include <string.h>
 
 int	main(void)
@@ -73,4 +117,5 @@ int	main(void)
 	a = strnstr(haystack, needle, len);
 	printf("Resultat Orifinal: %s \n", a);
 	return(0);
-}*/
+}
+ */
